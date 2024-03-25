e10ebf3faa4b8a823781f346d5d48831334406af
Imran Tan <83203909+imrantan@users.noreply.github.com>
2024-03-25 18:42:53 +0800

    Create settings.py
    Created a settings.py file. Removed the api keys.

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Telegram API Key
+API_KEY = "PLACE_YOUR_TELEGRAM_API_KEY_HERE"
+# Gemini API Key
+GOOGLE_API_KEY = 'PLACE_YOUR_GEMINI_API_KEY_HERE'
+# chat_history_db file
+chat_history_db = r'chat_history.db'
